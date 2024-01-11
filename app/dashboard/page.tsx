8fe6e71d5557fe93443b32fbfc045c96b0bb18c1
Swarnendu <swarnendumaity633@gmail.com>
2024-01-11 13:17:14 +0530

    Project

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,15 @@
 'use client'
 
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/dist/types/server'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
 function Page() {
-    const {} = getKindeServerSession()
+    const {getUser} = getKindeServerSession()
+    const user = getUser()
+
+    if(!user) redirect('/auth-callback?origin=dashboard');
+    
     
   return (
     <div>
