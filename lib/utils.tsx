81e976c18f5bbff0f015b3b3fe4d02a68c70c791
Swarnendu <swarnendumaity633@gmail.com>
2024-09-04 08:12:36 +0530

    First commit of Building Scalabel chat app

--- lib/utils.tsx
+++
@@ -1,9 +0,0 @@
-import { type ClassValue, clsx} from 'clsx'
-import React from 'react'
-import {twMerge} from 'tailwind-merge'
-
- export function cn(...input : ClassValue[]) {
-  return  twMerge(clsx(input))
-}
-
- 
